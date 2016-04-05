9634bfe01fc7271e083fa78df8e98cb02b51edad
Duran Audrey <audrey.duran@insa-lyon.fr>
2016-04-05 09:53:53 +0200

    Function to unify the blocks

--- prog.py
+++ prog.py
@@ -26,6 +26,7 @@ def read_File(file_name):
 def plot_DotPlot(file_name):
 	mat = read_File(file_name)
 	#contient 4 lignes: qstart qend sstart send
+	#Ordonne selon q_start
 
 	#N colonnes dans le fichier (1 start/1end = 1 ligne)
 	N = len(mat[0])
@@ -37,14 +38,11 @@ def plot_DotPlot(file_name):
 		#plt.plot([Q_start[i], Q_end[i]],[S_start[i], S_end[i]], color = 'blue')
 		#plt.xlim()
 		#plt.ylim()
-	compt_bar_mont = 0
-	compt_bar_desc = 0
 	print mat
 	for i in range(len(mat[0])-1):
 		#si suffisemment grand pour pas etre considere comme bruit pour paralogues = ceux dans la diago
 		if math.sqrt((mat[0][i]-mat[1][i])**2+(mat[2][i]-mat[3][i])**2) > 13000:
-			#plt.plot([mat[0][i], mat[1][i]],[mat[2][i], mat[3][i]], color = 'red')
-			compt_bar_mont = compt_bar_mont +1
+			plt.plot([mat[0][i], mat[1][i]],[mat[2][i], mat[3][i]], color = 'red')
 
 			matf[0].append(mat[0][i])
 			matf[1].append(mat[1][i])
@@ -54,8 +52,7 @@ def plot_DotPlot(file_name):
 		#filtre orthologues
 		if abs(mat[0][i]-mat[1][i+1]) > 1000:
 			if abs(mat[2][i]-mat[3][i+1]) < 1000:
-				#plt.plot([mat[0][i], mat[1][i]],[mat[2][i], mat[3][i]], color = 'green')
-				compt_bar_desc = compt_bar_desc +1
+				plt.plot([mat[0][i], mat[1][i]],[mat[2][i], mat[3][i]], color = 'green')
 
 				matf[0].append(mat[0][i])
 				matf[1].append(mat[1][i])
@@ -65,12 +62,32 @@ def plot_DotPlot(file_name):
 			#plt.plot([mat[0][i], mat[1][i]],[mat[2][i], mat[3][i]], color = 'blue')
 
 
-	# plt.xlabel('Q')
-	# plt.ylabel('S')
-	# plt.savefig("dotplot")
-	#plt.show()
-	print compt_bar_desc
-	print compt_bar_mont
+	plt.xlabel('Q')
+	plt.ylabel('S')
+	plt.savefig("dotplot")
+	plt.show()
 	return matf
 
-plot_DotPlot('Alignment.txt')
+
+def make_Blocks(mat):
+	#on cree une matrice dans laquelle on aura regroupe les blocs (segments consecutifs)
+	matb=[[] for i in range(4)]
+	#on parourt chaque ligne de la matrice
+	for i in range(len(mat[0][:])-1):
+		#si la distance entre
+		if (abs(mat[1][i]-mat[0][i+1]) < 1000) and (abs(mat[2][i+1]-mat[3][i] > 1000)):
+			matb[0].append(mat[0][i])
+			matb[1].append(mat[1][i+1])
+			matb[2].append(mat[2][i])
+			matb[3].append(mat[3][i+1])
+
+	print len(matb[0][:])-1
+	#print matb[0]
+	#print matb
+	for i in range(len(matb[0][:])-1):
+		plt.plot([matb[0][i], matb[1][i]],[matb[2][i], matb[3][i]], color = 'blue')
+	plt.show()
+
+
+m=plot_DotPlot('Alignment.txt')
+make_Blocks(m)
