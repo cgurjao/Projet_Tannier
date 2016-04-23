2cbbedad5be26083565887c8ead0acca2bd1c904
cgurjao <carino.gurjao@insa-lyon.fr>
2016-04-23 17:01:06 +0200

    Done looking for blocks

--- prog.py
+++ prog.py
@@ -34,16 +34,16 @@ def plot_DotPlot(file_name):
 	matf=[[] for i in range(4)]
 
 
-	#for i in range(N):
-		#plt.plot([Q_start[i], Q_end[i]],[S_start[i], S_end[i]], color = 'blue')
-		#plt.xlim()
-		#plt.ylim()
-	print mat
-	for i in range(len(mat[0])-1):
+	#plt.plot([mat[0], mat[1]],[mat[2], mat[3]], color = 'green')
+	#plt.xlabel('Q')
+	#plt.ylabel('S')
+	#plt.savefig("Unfiltered")
+	#plt.close()
+	
+	for i in range(N-1):
 		#si suffisemment grand pour pas etre considere comme bruit pour paralogues = ceux dans la diago
 		if math.sqrt((mat[0][i]-mat[1][i])**2+(mat[2][i]-mat[3][i])**2) > 13000:
-			plt.plot([mat[0][i], mat[1][i]],[mat[2][i], mat[3][i]], color = 'red')
-
+			#plt.plot([mat[0][i], mat[1][i]],[mat[2][i], mat[3][i]], color = 'red')
 			matf[0].append(mat[0][i])
 			matf[1].append(mat[1][i])
 			matf[2].append(mat[2][i])
@@ -52,8 +52,7 @@ def plot_DotPlot(file_name):
 		#filtre orthologues
 		if abs(mat[0][i]-mat[1][i+1]) > 1000:
 			if abs(mat[2][i]-mat[3][i+1]) < 1000:
-				plt.plot([mat[0][i], mat[1][i]],[mat[2][i], mat[3][i]], color = 'green')
-
+				#plt.plot([mat[0][i], mat[1][i]],[mat[2][i], mat[3][i]], color = 'green')
 				matf[0].append(mat[0][i])
 				matf[1].append(mat[1][i])
 				matf[2].append(mat[2][i])
@@ -62,76 +61,54 @@ def plot_DotPlot(file_name):
 			#plt.plot([mat[0][i], mat[1][i]],[mat[2][i], mat[3][i]], color = 'blue')
 
 
-	# plt.xlabel('Q')
-	# plt.ylabel('S')
-	# plt.savefig("dotplot")
-	# #plt.show()
+	plt.xlabel('Q')
+	plt.ylabel('S')
+	plt.savefig("Filtered")
+	plt.close()
 	return matf
 
 
-def make_Blocks(mat):
-	#on cree une matrice dans laquelle on aura regroupe les blocs (segments consecutifs)
-	matb=[[] for i in range(4)]
+def search_blocks(mat):
+	N = len(mat[0])
+	i = 0
+	blocks_Q_start = []
+	blocks_Q_end = []
+	blocks_S_start = []
+	blocks_S_end = []
+	blocks_type = []
+	while i < N-1:
+		if abs(mat[0][i+1] - mat[0][i]) < 2000:
+			blocks_Q_start.append(mat[0][i])
+			blocks_S_start.append(mat[2][i])
+			while (abs(mat[0][i+1] - mat[0][i]) < 15000)  and (abs(mat[3][i+1] - mat[3][i]) < 200000) and (i < N-2):
+				i = i+1
+			blocks_Q_end.append(mat[1][i])
+			blocks_S_end.append(mat[3][i])
+			blocks_type.append(0)
+		i = i +1
 
-	#on parourt chaque ligne de la matrice
-	i=0
-	while i < (len(mat[0][:])-1):
-		print "i=",i
-		#orthologue (=diago) : qend(1) de i proche de qstart(0) i+1
-		#sstart (2) de i+1 proche de send(3) de i
 
-		#difference paralogue / orthologue : signe de la difference ssend[i]-start[i+1]
+	i = 0
+	while i < N-1:
+		if math.sqrt((mat[0][i]-mat[1][i])**2+(mat[2][i]-mat[3][i])**2) > 100000:
+			blocks_Q_start.append(mat[0][i])
+			blocks_S_start.append(mat[2][i])
+			while math.sqrt((mat[0][i]-mat[1][i])**2+(mat[2][i]-mat[3][i])**2) > 100000 and (i < N-2) and (abs(mat[3][i+1] - mat[3][i]) < 300000):
+				i = i+1
+			blocks_Q_end.append(mat[1][i])
+			blocks_S_end.append(mat[3][i])
+			blocks_type.append(0)
+		i = i +1
 
-		#paralogues (diago)
-		if((abs(mat[1][i]-mat[0][i+1]) < 1000) and ((mat[2][i+1]-mat[3][i]) < 500)):
-			print "----------- rentre"
-			j=i+1
-			while j<(len(mat[0][:])-1):
-			#on cherche jusqu'ou va le segment
-				if ((abs(mat[1][j]-mat[0][j+1]) < 1000) and ((mat[2][j+1]-mat[3][j]) < 500)):
-					j=j+1
-					print "---------j=",j
-				else:
-					break
 
-			matb[0].append(mat[0][i])
-			matb[1].append(mat[1][j])
-			matb[2].append(mat[2][i])
-			matb[3].append(mat[3][j])
-
-			i=j
-
-		#orthologues
-		if ((abs(mat[1][i]-mat[0][i+1]) < 1000) and ((mat[3][i]-mat[2][i+1]) < 500)):
-			print "----------- rentre 2"
-			j=i+1
-			while j<(len(mat[0][:])-1):
-			#on cherche jusqu'ou va le segment
-				if ((abs(mat[1][j]-mat[0][j+1]) < 1000) and ((mat[3][j]-mat[2][j+1]) < 500)):
-					j=j+1
-					print "---------j=",j
-				else:
-					break
-
-			matb[0].append(mat[0][i])
-			matb[1].append(mat[1][j])
-			matb[2].append(mat[2][i])
-			matb[3].append(mat[3][j])
-
-			i=j
-
-		i=i+1
-
-	print "taille matb:",len(matb[0][:])
-	#print matb[0]
-	#print matb
-	for i in range(len(matb[0][:])-1):
-		plt.plot([matb[0][i], matb[1][i]],[matb[2][i], matb[3][i]], color = 'blue')
+	blocks = np.array([blocks_Q_start,blocks_Q_end,blocks_S_start, blocks_S_end])
+	for i in range(len(blocks[0])):
+		plt.plot([blocks[0][i], blocks[1][i]],[blocks[2][i], blocks[3][i]], color = 'red')
 	plt.show()
-
-	#ok pour les othologues
-
+	return blocks
+	#### blocks contient:
+	# - les quatres premières lignes sont respectivements Q_start, Q_end, S_start, S_end
+	# - la dernière ligne décrit le type de bloc : 0 pour les paralogues et 1 pour les orthologues
 
 m=plot_DotPlot('Alignment.txt')
-print "taille matf", len(m[0][:])
-make_Blocks(m)
+search_blocks(m)
